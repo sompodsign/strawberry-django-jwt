2bb412f407f1f75eebb1bca40b217fd4d6231b6c
Shampad Sharkar <shampad@anymindgroup.com>
2024-09-27 09:58:21 +0700

    Remove unnecessary imports and commented-out code related to JWT authentication and viewsets in the URLs configuration.

--- tests/example_app/urls.py
+++ tests/example_app/urls.py
@@ -1,15 +1,9 @@
 from django.contrib.auth.models import User
-from django.urls import include, path, re_path
-from rest_framework import serializers, viewsets
-from rest_framework.permissions import IsAuthenticated
+from django.urls import re_path
+from rest_framework import serializers
 
-# Serializers define the API representation.
-from rest_framework.routers import DefaultRouter
 from strawberry.django.views import GraphQLView,AsyncGraphQLView
 
-from strawberry_django_jwt.decorators import jwt_cookie
-from strawberry_django_jwt.views import AsyncStatusHandlingGraphQLView as AGQLView
-from strawberry_django_jwt.views import StatusHandlingGraphQLView as GQLView
 from tests.example_app.schema import schema, sync_schema
 
 
@@ -18,25 +12,10 @@ class UserSerializer(serializers.HyperlinkedModelSerializer):
         model = User
         fields = ["url", "username", "email", "is_staff"]
 
-
-# # ViewSets define the view behavior.
-# class UserViewSet(viewsets.ModelViewSet):
-#     permission_classes = [IsAuthenticated]
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#
-#
-# router = DefaultRouter()
-# router.register(r"", UserViewSet, basename="user")
-
-# Wire up our API using automatic URL routing.
-# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
     re_path(r"^graphql/?$", AsyncGraphQLView.as_view(schema=schema), name="graphql"),
     re_path(
         r"^sync-graphql/?$", GraphQLView.as_view(schema=sync_schema),
         name="sync_graphql",
     ),
-    # path("users", include(router.urls)),
-    # path("api-auth/", include("rest_framework.urls", namespace="rest_framework")),
 ]
